bd1b4e2eb0a125c3794ed00d2b4fa34c1b85a52c
deadlium <uddeshjais.21@gmail.com>
2024-12-13 17:32:47 +0530

    feat: add CosmWasm contract interaction script and update .gitignore
    - Added  for deploying and interacting with CosmWasm contracts.
    - Updated  to include .
    - Structured the project files for better readability and modularity.

---
+++ learning/script/wasm.sh
@@ -0,0 +1,46 @@
+JUNCTION="./artifacts/junctiond"
+CHAIN_ID="junction"
+KEY_NAME="okok"
+KEYRING_BACKEND="--keyring-backend test"
+WASM="artifacts/learning.wasm"
+CONTRACT_ADDRESS="air14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9snx9gzt"
+
+$JUNCTION tx wasm store $WASM \
+  --from $KEY_NAME \
+  --chain-id $CHAIN_ID \
+  $KEYRING_BACKEND \
+  --gas auto \
+  --gas-adjustment 1.3 \
+  --gas-prices 0.025amf \
+  -y
+
+sleep 5s
+
+$JUNCTION tx wasm instantiate 1 '{}' \
+  --from $KEY_NAME \
+  --label "Counter Contract" \
+  --chain-id $CHAIN_ID \
+  $KEYRING_BACKEND \
+  --no-admin \
+  --gas auto \
+  --gas-adjustment 1.3 \
+  --gas-prices 0.025amf \
+  -y
+
+sleep 5s
+
+$JUNCTION tx wasm execute $CONTRACT_ADDRESS '{"Increment": {}}' \
+  --from $KEY_NAME \
+  --chain-id $CHAIN_ID \
+  $KEYRING_BACKEND \
+  --gas auto \
+  --gas-adjustment 1.3 \
+  --gas-prices 0.025amf \
+  -y
+
+sleep 5s
+
+$JUNCTION query wasm contract-state smart $CONTRACT_ADDRESS '{"GetCount": {}}' \
+  --chain-id $CHAIN_ID \
+  --node tcp://0.0.0.0:26657
+
